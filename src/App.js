865ad3e99c9e94a22b5489112cb43bbbd9926752
giovanni1129 <giovanni.u@hotmail.com>
2020-03-06 10:45:57 -0600

    More-Forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MoreForm from "./components/MoreForm";
 
-function App() {
+const App = () => {
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password:"",
+    confirmPassword:"",
+  })
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setFormState({
+      ...formState,
+      validated:false
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {formState.firstName.length < 2 ?  "First Name must be 2 characters long" : ""}<br/>
+        {formState.lastName.length < 2 ?  "Last Name must be 2 characers long" : ""}<br/>
+        {formState.email.length < 2 ?  "email is invalid" : ""}<br/>
+        {formState.password.length < 8 ?  "Password must be 8 characters long" : ""}<br/>
+        {formState.confirmPassword != formState.password ? "passwords dont match" : ""}
+      </div>
+      
+      <MoreForm
+      onChangeHandler = {onChangeHandler}
+      onSubmitHandler = {onSubmitHandler}
+      />
+      <p>your From Data</p>
+  <p>First Name: {formState.firstName}</p>
+  <p>Last Name: {formState.lastName}</p>
+  <p>Email: {formState.email}</p>
+  <p>Password: {formState.password}</p>
+  <p>Confirm Password: {formState.confirmPassword}</p>
     </div>
-  );
+  )
 }
 
 export default App;
